43ef2d17f939c14da937ce7cc57e6fa2738898ef
danutavram <avramnicolaedan@gmail.com>
2024-03-07 21:39:44 +0200

    Doctrine Relationships

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,23 +3,35 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Entity\Video;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Doctrine\Persistence\ManagerRegistry;
 
 class DefaultController extends AbstractController
 {
     #[Route('/home', name: 'home')]
-    public function index(ManagerRegistry $doctrine): Response
+    public function index(UserRepository $userRepository, EntityManagerInterface $entityManager)
     {
-        $entityManager = $doctrine->getManager();
-        
-        $users = $entityManager->getRepository(User::class)->findAll();
+        $user = new User();
+        $user->setName('Robert');
+
+        for ($i = 1; $i <= 3; $i++) {
+            $video = new Video();
+            $video->setTitle('Video Title - ' . $i);
+            $user->addVideo($video);
+        }
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        dump('Created a video with the id of ' . $video->getId());
+        dump('Created a user with the id of ' . $user->getId());
 
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
-            'users' => $users,
         ]);
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,18 +3,32 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use DateTime;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\HasLifecycleCallbacks]
 class User
 {
+
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
     private ?string $name = null;
+
+    #[ORM\OneToMany(targetEntity: Video::class, mappedBy: 'user', cascade: ['persist'])]
+    private Collection $videos;
+
+    public function __construct()
+    {
+        $this->videos = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -29,6 +43,36 @@ class User
     public function setName(string $name): self
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Video>
+     */
+    public function getVideos(): Collection
+    {
+        return $this->videos;
+    }
+
+    public function addVideo(Video $video): static
+    {
+        if (!$this->videos->contains($video)) {
+            $this->videos->add($video);
+            $video->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeVideo(Video $video): static
+    {
+        if ($this->videos->removeElement($video)) {
+            // set the owning side to null (unless already changed)
+            if ($video->getUser() === $this) {
+                $video->setUser(null);
+            }
+        }
 
         return $this;
     }
